10b1dab6968d4b0afe506b207a6231e8eb5fc3f3
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-10-18 18:10:58 +0900

    practice3

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter);
+ 
 int main(int argc, char *argv[])
 {
-    int i;
-    for (i=0;i<5;i++)
-    {
-        int temp = 1;
-        printf("temp=%d\n", temp);
-        temp++;
-    }
+    int i=10;
+    
+    printf("함수 호출 전 i=%d\n", i);
+    i =inc(i);//반환값 이용해야 10, 11이 나 
+    
+    printf(" 함수 호출  i=%d\n", i);
     
   system("PAUSE");	
   return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter; //매개변수->끝나면 소 
 }
