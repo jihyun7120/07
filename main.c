ea813a868d2ce2e8f51b9f183e9b3cba031589d7
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-10-18 18:47:19 +0900

    practice6_1

--- main.c
+++ main.c
@@ -14,12 +14,3 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-void sub(void)
-{
-    int auto_count=0;
-    static int static_count=0;
-    auto_count++;
-    static_count++;
-    printf("auto_count=%d\n", auto_count);
-    printf("static_count=%d\n", static_count);
-}
